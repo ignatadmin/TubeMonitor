96006c4955c123885335f5cd3bc0457ed0c5ffec
ignatadmin <heteryga@gmail.com>
2024-06-22 08:52:28 +0500

    simplified all models

--- analytic/models.py
+++ analytic/models.py
@@ -1,39 +1,24 @@
 from django.db import models
 
 
-class VideosByViews(models.Model):
-    title = models.CharField(default=None, null=True, verbose_name="123")
-    thumbnail = models.CharField(default=None, null=True, verbose_name="123")
-    view_count = models.IntegerField(default=None, null=True, verbose_name="123")
-    channel_icon = models.CharField(default=None, null=True, verbose_name="123")
-    channel_id = models.CharField(default=None, null=True, verbose_name="123")
-    channel_title = models.CharField(default=None, null=True, verbose_name="123")
+class ListTopVideos(models.Model):
+    title = models.CharField(default=None, null=True, verbose_name="название")
+    thumbnail = models.CharField(default=None, null=True, verbose_name="миниатюра")
+    view_count = models.IntegerField(default=None, null=True, verbose_name="число просмотров")
+    channel_icon = models.CharField(default=None, null=True, verbose_name="иконка канала")
+    channel_id = models.CharField(default=None, null=True, verbose_name="айди канала")
+    channel_title = models.CharField(default=None, null=True, verbose_name="название канала")
+    made_for_kids = models.BooleanField(default=False, null=True, verbose_name="контент для детей")
 
     def __str__(self):
-        return f'{self.pk} - {self.title} - {self.view_count}'
+        return f'{self.pk} - {self.title} - {self.view_count} - {self.made_for_kids}'
 
     class Meta:
-        verbose_name = "топ 100 видео по просмотрам"
-        verbose_name_plural = "топ 100 видео по просмотрам"
+        verbose_name = "топ видео по просмотрам"
+        verbose_name_plural = "топ видео по просмотрам"
 
 
-class VideosByViewsNotKids(models.Model):
-    title = models.CharField(default=None, null=True, verbose_name="123")
-    thumbnail = models.CharField(default=None, null=True, verbose_name="123")
-    view_count = models.IntegerField(default=None, null=True, verbose_name="123")
-    channel_icon = models.CharField(default=None, null=True, verbose_name="123")
-    channel_id = models.CharField(default=None, null=True, verbose_name="123")
-    channel_title = models.CharField(default=None, null=True, verbose_name="123")
-
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.view_count}'
-
-    class Meta:
-        verbose_name = "топ 100 видео по просмотрам кроме для детей"
-        verbose_name_plural = "топ 100 видео по просмотрам кроме для детей"
-
-
-class ChannelBaseModel(models.Model):
+class ListTopChannels(models.Model):
     title = models.CharField(default=None, null=True, verbose_name="название")
     channel_id = models.CharField(max_length=24, default=None, null=True, verbose_name="айди канала")
     thumbnails = models.CharField(default=None, null=True, verbose_name="миниатюра")
@@ -42,68 +27,9 @@ class ChannelBaseModel(models.Model):
     video_count = models.IntegerField(default=None, null=True, verbose_name="число видео")
     country = models.CharField(default=None, null=True, verbose_name="страна")
 
-    class Meta:
-        abstract = True
-
-
-class TopChannels(ChannelBaseModel):
     def __str__(self):
         return f'{self.pk} - {self.title} - {self.channel_id}'
 
     class Meta:
         verbose_name = "список топ каналов"
         verbose_name_plural = "список топ каналов"
-
-
-class ChannelsBySubs(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.subscriber_count}'
-
-    class Meta:
-        verbose_name = "топ 100 каналов по подписчикам"
-        verbose_name_plural = "топ 100 каналов по подписчикам"
-
-
-class ChannelsBySubsRu(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.subscriber_count}'
-
-    class Meta:
-        verbose_name = "топ 100 RU каналов по подписчикам"
-        verbose_name_plural = "топ 100 RU аналов по подписчикам"
-
-
-class ChannelsByViews(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.view_count}'
-
-    class Meta:
-        verbose_name = "топ 100 каналов по просмотрам"
-        verbose_name_plural = "топ 100 каналов по просмотрам"
-
-
-class ChannelsByViewsRu(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.view_count}'
-
-    class Meta:
-        verbose_name = "топ 100 RU каналов по просмотрам"
-        verbose_name_plural = "топ 100 RU каналов по просмотрам"
-
-
-class ChannelsByVideos(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.video_count}'
-
-    class Meta:
-        verbose_name = "топ 100 каналов по количеству видео"
-        verbose_name_plural = "топ 100 каналов по количеству видео"
-
-
-class ChannelsByVideosRu(ChannelBaseModel):
-    def __str__(self):
-        return f'{self.pk} - {self.title} - {self.video_count}'
-
-    class Meta:
-        verbose_name = "топ 100 RU каналов по количеству видео"
-        verbose_name_plural = "топ 100 RU каналов по количеству видео"
--- analytic/services.py
+++ analytic/services.py
@@ -51,139 +51,69 @@ def get_video_data(parsed_url_str):
     return video_data
 
 
-class UpdateVideosToplists:
-    def __init__(self):
-        self.api_key = settings.API_KEY
-        self.api_service_name = 'youtube'
-        self.api_version = 'v3'
-        self.playlist_id = 'PL11E57E1166929B60'
-        self.api_request = build(self.api_service_name, self.api_version, developerKey=self.api_key)
-
-    def _fetch_playlist_items(self):
-        return self.api_request.playlistItems().list(
+def update_video_toplist():
+    key = settings.API_KEY
+    api_request = build('youtube', 'v3', developerKey=key)
+    playlist_id = 'PL11E57E1166929B60'
+    counter = 0
+    ListTopVideos.objects.all().delete()
+    while counter < 100:
+        playlist_items = api_request.playlistItems().list(
             part='snippet',
-            playlistId=self.playlist_id,
-            maxResults=100
-        ).execute()['items']
-
-    def _fetch_video_details(self, video_id):
-        video_response = self.api_request.videos().list(
-            part='snippet,statistics,status',
-            id=video_id
+            playlistId=playlist_id,
+            maxResults=50,
         ).execute()
-        return video_response['items'][0] if video_response['items'] else None
 
-    def _fetch_channel_details(self, channel_id):
-        channel_response = self.api_request.channels().list(
-            part='snippet',
-            id=channel_id
-        ).execute()
-        return channel_response['items'][0] if channel_response['items'] else None
+        for item in playlist_items['items']:
+            video_id = item['snippet']['resourceId']['videoId']
+            request_data = api_request.videos().list(
+                part='snippet,contentDetails,statistics,status',
+                id=video_id
+            ).execute()
 
-    def _update_videos(self, videos, model_class):
-        model_class.objects.all().delete()
+            channel_id = request_data['items'][0]['snippet']['channelId'],
+            title = request_data['items'][0]['snippet']['title'],
+            thumbnail = request_data['items'][0]['snippet']['thumbnails']['maxres']['url'],
+            channel_title = request_data['items'][0]['snippet']['channelTitle'],
+            made_for_kids = request_data['items'][0]['status']['madeForKids'],
+            view_count = request_data['items'][0]['statistics']['viewCount'],
 
-        for video in videos:
-            video_id = video['snippet']['resourceId']['videoId']
-            video_title = video['snippet']['title']
-            video_thumbnail = video['snippet']['thumbnails']['maxres']['url']
-
-            video_details = self._fetch_video_details(video_id)
-            if video_details:
-                stats = video_details['statistics']
-                view_count = int(stats['viewCount'])
-                channel_id = video['snippet']['channelId']
-
-                channel_details = self._fetch_channel_details(channel_id)
-                if channel_details:
-                    channel_title = channel_details['snippet']['title']
-                    channel_icon = channel_details['snippet']['thumbnails']['default']['url']
-
-                    model_class.objects.create(
-                        title=video_title,
-                        thumbnail=video_thumbnail,
-                        view_count=view_count,
-                        channel_icon=channel_icon,
-                        channel_id=channel_id,
-                        channel_title=channel_title
-                    )
-
-    def update_data_toplist_videos_not_for_kids(self):
-        playlist_items = self._fetch_playlist_items()
-
-        filtered_videos = [
-            video for video in playlist_items
-            if video['status']['madeForKids'] == False
-        ]
-
-        self._update_videos(filtered_videos, VideosByViewsNotKids)
-
-    def update_data_toplist_videos(self):
-        playlist_items = self._fetch_playlist_items()
-
-        self._update_videos(playlist_items, VideosByViews)
-
-    def update_video_toplists(self):
-        self.update_data_toplist_videos_not_for_kids()
-        self.update_data_toplist_videos()
-
-
-class UpdateChannelsToplists():
-    def update_data_toplist_channels(self):
-        """обноление общего списка топ каналов"""
-        queryset = TopChannels.objects.all()
-
-        for top_channel in queryset:
-            key = settings.API_KEY
-            api_request = build('youtube', 'v3', developerKey=key)
             request_data = api_request.channels().list(
-                part='snippet,statistics,status',
-                id=top_channel.channel_id
+                part='snippet',
+                id=channel_id
+            ).execute()
+            channel_icon = request_data['items'][0]['snippet']['thumbnails']['maxres']['url']
+
+            ListTopVideos.objects.create(
+                title=title,
+                thumbnail=thumbnail,
+                view_count=view_count,
+                channel_icon=channel_icon,
+                channel_id=channel_id,
+                channel_title=channel_title,
+                made_for_kids=made_for_kids
             )
-            response = request_data.execute()
-            channel_data = response['items'][0] if 'items' in response else None
-            if channel_data:
-                top_channel.title = channel_data['snippet']['title']
-                top_channel.thumbnails = channel_data['snippet']['thumbnails']['default']['url']
-                top_channel.viewCount = int(channel_data['statistics']['viewCount'])
-                top_channel.subscriberCount = int(channel_data['statistics']['subscriberCount'])
-                top_channel.videoCount = int(channel_data['statistics']['videoCount'])
-                top_channel.country = channel_data['snippet'].get('country', '')
-                top_channel.save()
+            if not made_for_kids:
+                counter += 1
 
-    def update_channels_data(self, model_cls, country_filter=None):
-        """обновление талблиц топ каналов по критериям"""
-        top_channels = TopChannels.objects.all()
-        if country_filter:
-            top_channels = top_channels.filter(country=country_filter)
 
-        top_channels = top_channels.order_by(
-            '-subscriberCount' if model_cls == ChannelsBySubs else
-            '-viewCount' if model_cls == ChannelsByViews else
-            '-videoCount'
-        )[:100]
-        model_cls.objects.all().delete()
+def update_channel_toplist():
+    queryset = ListTopChannels.objects.all()
 
-        for channel in top_channels:
-            model_cls.objects.create(
-                title=channel.title,
-                channel_id=channel.channel_id,
-                thumbnails=channel.thumbnails,
-                viewCount=channel.viewCount,
-                subscriberCount=channel.subscriberCount,
-                videoCount=channel.videoCount,
-                country=channel.country
-            )
-
-    def update_channel_toplists(self):
-        models_to_update = [
-            (ChannelsByVideos, None),
-            (ChannelsByVideos, 'RU'),
-            (ChannelsByViews, None),
-            (ChannelsByViews, 'RU'),
-            (ChannelsBySubs, None),
-            (ChannelsBySubs, 'RU'),
-        ]
-        self.update_data_toplist_channels()
-        for model_cls, country_filter in models_to_update:
-            self.update_channels_data(model_cls, country_filter)
+    for top_channel in queryset:
+        key = settings.API_KEY
+        api_request = build('youtube', 'v3', developerKey=key)
+        request_data = api_request.channels().list(
+            part='snippet,statistics,status',
+            id=top_channel.channel_id
+        )
+        response = request_data.execute()
+        channel_data = response['items'][0] if 'items' in response else None
+        if channel_data:
+            top_channel.title = channel_data['snippet']['title']
+            top_channel.thumbnails = channel_data['snippet']['thumbnails']['default']['url']
+            top_channel.viewCount = int(channel_data['statistics']['viewCount'])
+            top_channel.subscriberCount = int(channel_data['statistics']['subscriberCount'])
+            top_channel.videoCount = int(channel_data['statistics']['videoCount'])
+            top_channel.country = channel_data['snippet'].get('country', '')
+            top_channel.save()
