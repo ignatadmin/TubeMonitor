8df6911094e0b536c66748ccb1e71a1e024582bd
ignatadmin <heteryga@gmail.com>
2024-06-23 20:59:18 +0500

    Add fields arguments

--- analytic/models.py
+++ analytic/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class ListTopVideos(models.Model):
-    title = models.CharField(default=None, null=True, verbose_name="название")
-    thumbnail = models.CharField(default=None, null=True, verbose_name="миниатюра")
+    title = models.CharField(max_length=100, default=None, null=True, verbose_name="название")
+    thumbnail = models.CharField( max_length=200, default=None, null=True, verbose_name="миниатюра")
     view_count = models.BigIntegerField(default=None, null=True, verbose_name="число просмотров")
-    channel_icon = models.CharField(default=None, null=True, verbose_name="иконка канала")
-    channel_id = models.CharField(default=None, null=True, verbose_name="айди канала")
-    channel_title = models.CharField(default=None, null=True, verbose_name="название канала")
+    channel_icon = models.CharField(max_length=200, default=None, null=True, verbose_name="иконка канала")
+    channel_id = models.CharField(max_length=24, default=None, null=True, verbose_name="айди канала")
+    channel_title = models.CharField(max_length=100, default=None, null=True, verbose_name="название канала")
     made_for_kids = models.BooleanField(default=False, null=True, verbose_name="контент для детей")
 
     def __str__(self):
@@ -19,13 +19,13 @@ class ListTopVideos(models.Model):
 
 
 class ListTopChannels(models.Model):
-    title = models.CharField(default=None, null=True, verbose_name="название")
+    title = models.CharField(max_length=100, default=None, null=True, verbose_name="название")
     channel_id = models.CharField(max_length=24, default=None, null=True, verbose_name="айди канала")
-    thumbnails = models.CharField(default=None, null=True, verbose_name="миниатюра")
+    channel_icon = models.CharField(max_length=200, default=None, null=True, verbose_name="миниатюра")
     view_count = models.BigIntegerField(default=None, null=True, verbose_name="число просмотров")
     subscriber_count = models.BigIntegerField(default=None, null=True, verbose_name="число подписчиков")
     video_count = models.BigIntegerField(default=None, null=True, verbose_name="число видео")
-    country = models.CharField(default=None, null=True, verbose_name="страна")
+    country = models.CharField(max_length=100, default=None, null=True, verbose_name="страна")
 
     def __str__(self):
         return f'{self.pk} - {self.title} - {self.channel_id}'
