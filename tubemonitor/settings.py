75621e0efc152d6cfc5c4fcc626c59d43b49ea95
ignatadmin <heteryga@gmail.com>
2024-06-28 07:52:27 +0500

    Add Debug Toolbar

--- tubemonitor/settings.py
+++ tubemonitor/settings.py
@@ -14,6 +14,8 @@ DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
+INTERNAL_IPS = ['127.0.0.1']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- tubemonitor/urls.py
+++ tubemonitor/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('analytic.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
+
 ]
 
