92fa9d1876bca18b33461658458034049180889f
ignatadmin <heteryga@gmail.com>
2024-07-02 12:59:58 +0500

    Fix sorting bug

--- analytic/views.py
+++ analytic/views.py
@@ -29,7 +29,7 @@ def channel_data(request):
     """ По URL получает данные канала из YouTube API, и отображает их"""
     parsed_url_str = request.COOKIES.get('parsed_url')
     channel_data = get_channel_data(parsed_url_str)
-    return render(request,'channel.html', {'channel_data': channel_data})
+    return render(request, 'channel.html', {'channel_data': channel_data})
 
 
 def video_data(request):
@@ -43,6 +43,7 @@ def video_data(request):
 
 class TopListVideos(View):
     """ Представление для отображения списка топовых видео """
+
     def get_videos_data(self, request, for_kids: bool = True):
         length_list = 100 if request.user.is_active else 15
         try:
@@ -57,12 +58,15 @@ class TopListVideos(View):
         return self.get_videos_data(request)
 
     def post(self, request):
-        for_kids = request.POST.get('for_kids') == 'true'
+        for_kids = request.POST.get('for_kids')
+        if for_kids:
+            for_kids = True
         return self.get_videos_data(request, for_kids)
 
 
 class TopListChannels(View):
     """ Представление для отображения списка топовых каналов """
+
     def get_channels_data(self, request, for_kids: bool, only_ru: bool, sort: str) -> HttpResponse:
         length_list = 100 if request.user.is_active else 15
         channels_data_func = {
@@ -84,6 +88,15 @@ class TopListChannels(View):
 
     def post(self, request):
         sort = request.GET.get('sort', 'subscribers')
-        for_kids = request.POST.get('for_kids') == 'true'
-        only_ru = request.POST.get('only_ru') == 'true'
+        for_kids = request.POST.get('for_kids')
+        only_ru = request.POST.get('only_ru')
+        if for_kids:
+            for_kids = True
+        else:
+            for_kids = False
+        if only_ru:
+            only_ru = True
+            for_kids = True
+        else:
+            only_ru = False
         return self.get_channels_data(request, for_kids=for_kids, only_ru=only_ru, sort=sort)
