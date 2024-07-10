15fe6be85b90bc8fcbd7f8faca7e3799fe3d5878
ignatadmin <heteryga@gmail.com>
2024-07-10 12:22:44 +0500

    Add update toplists task start time

--- tubemonitor/celery.py
+++ tubemonitor/celery.py
@@ -9,9 +9,10 @@ app = Celery('tubemonitor')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+"""Выполняет таск в 07:00 мск ежедневно"""
 app.conf.beat_schedule = {
-    'update_toplists_every_10_min': {
+    'update_toplists_every_day': {
         'task': 'analytic.tasks.update_toplists_task',
-        'schedule': crontab(minute='*/10')
+        'schedule': crontab(hour=7, minute=0, day_of_week='*'),
     },
 }
